49ed2cc9848aca3d030165145502560f46eed93a
Jinko Graham <jgraham@sfu.ca>
2019-07-29 22:41:13 -0700

    clean up conda_install_local()

--- PythonPackages/conda_install_local.R
+++ PythonPackages/conda_install_local.R
@@ -1,16 +1,15 @@
 conda_install_local <- function(env,pkg,dir) {
   # Modified version of reticulate's conda_install() function that
-  # installs package "pkg" into the conda environment "env" from a file
-  # dowloaded into the directory "dir".
+  # installs package "pkg" into the conda environment "env" from a package
+  # in the directory "dir".
   conda <- conda_binary()
   env <- reticulate:::condaenv_resolve(env)
   python <- conda_python(envname=env)
   args <- reticulate:::conda_args("install",env)
   args <- c(args,"-c",paste0("file:/",dir),pkg)
-  # result <- system2(conda,shQuote(args))
-  result <- system(paste(conda,"install -n Stat840 -c file://Users/mcneney/Downloads/ pyslim"))
+  result <- system2(conda,shQuote(args)
   if (result != 0L) {
-    stop("Error ", result, " occurred installing pyslim into conda environment ", 
+    stop("Error ", result, " occurred installing ",pkg," into conda environment ", 
          env, call. = FALSE)
   }
   invisible(NULL)
