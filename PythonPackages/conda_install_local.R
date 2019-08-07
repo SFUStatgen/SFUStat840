c08d0617d82f2f4d4e8f23e15b314eca41c7b22c
jinkog <jgraham@sfu.ca>
2019-08-06 23:26:42 -0700

    make conda check conda-forge

--- PythonPackages/conda_install_local.R
+++ PythonPackages/conda_install_local.R
@@ -6,7 +6,9 @@ conda_install_local <- function(env,pkg,dir) {
   env <- reticulate:::condaenv_resolve(env)
   python <- conda_python(envname=env)
   args <- reticulate:::conda_args("install",env)
-  args <- c(args,"-c",paste0("file:/",dir),pkg)
+  args <- c(args,"-c",paste0("file:/",dir),
+            "-c","conda-forge",
+            pkg)
   result <- system2(conda,shQuote(args))
   if (result != 0L) {
     stop("Error ", result, " occurred installing ",pkg," into conda environment ", 
