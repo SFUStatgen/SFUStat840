173bed3947e549a7740eb57d55ab4a58e1846873
jinkog <jgraham@sfu.ca>
2019-10-31 16:02:50 -0700

    testing installation of pyslim

--- PythonPackages/conda_install_local.R
+++ PythonPackages/conda_install_local.R
@@ -1,10 +1,10 @@
-conda_install_local <- function(env,pkg,dir) {
+conda_install_local <- function(env,pkg,dir,conda="auto") {
   # Modified version of reticulate's conda_install() function that
   # installs package "pkg" into the conda environment "env" from a package
-  # in the directory "dir".
-  conda <- conda_binary()
+  # in the directory "dir" using the conda executable "conda".
+  conda <- conda_binary(conda)
   env <- reticulate:::condaenv_resolve(env)
-  python <- conda_python(envname=env)
+  python <- conda_python(envname=env,conda)
   args <- reticulate:::conda_args("install",env)
   args <- c(args,"-c",paste0("file:/",dir),
             "-c","conda-forge",
